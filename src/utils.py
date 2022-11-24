cdc2d98429f1aaa22d39467320a99a17012c4792
DominQu <dominik.kuczkowski00@gmail.com>
2022-11-24 22:53:58 +0100

    Add method for loading custom sequence

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,9 @@ import random
 
 import torch
 from numpy.random import default_rng
+from PIL import Image
+import torchvision.transforms as transforms
+
 
 def set_global_seed(seed=777):
     """Set global seed for modules that have random generators"""
@@ -23,3 +26,20 @@ def load_params(config_file: Path) -> dict:
             config = json.load(f)
         logging.info("Config file loaded succesfully")
         return config
+
+def load_sequence(img_dir: Path, seq_len: int=20, img_size: int=64) -> list:
+    """Load frame sequence from directory"""
+    if not img_dir.is_dir():
+        raise ValueError("Given path is not a directory!")
+    transform = transforms.Compose([transforms.Resize((img_size, img_size)), transforms.ToTensor()])
+    
+    frames = img_dir.glob('*')
+    seq = []
+    for frame in frames:
+        img = Image.open(str(frame))
+        seq.append(transform(img))
+
+    if len(seq) < seq_len+2:
+        raise ValueError("There isn't enough frames in specified directory")
+
+    return seq[:seq_len+2]
